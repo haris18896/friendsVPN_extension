55f28ebe068b72c14cb76c1c06df9639717564c9
haris18896 <haris18896@gmail.com>
2022-07-18 14:38:08 +0500

    Next Js added to plasmo, its working, now working with extension manifest problem

---
+++ friends-vpn/pages/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Custom404() {
+  return (
+    <div className='customError'>
+      <p>
+        <strong>404</strong> | page not found
+      </p>
+    </div>
+  )
+}
+
+export default Custom404
